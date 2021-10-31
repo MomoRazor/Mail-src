3082e3252254202a51ea8b6eb076b7dec48f88a2
MomoRazor <maurovic.cachia@gmail.com>
2021-10-31 23:54:58 +0100

    Middleware fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,24 @@ import { SERVICE_PROVIDER_DOMAINS } from './enviornment';
 export const bouncer = (req: Request, res: Response, next: NextFunction) => {
     if (SERVICE_PROVIDER_DOMAINS) {
         const verifiedDomains = SERVICE_PROVIDER_DOMAINS.split(',');
-        const originUrl = req.header('Origin') || req.get('host') || req.hostname;
+        console.log(
+            req.header('Origin'),
+            req.get('host'),
+            req.get('origin'),
+            req.headers.host,
+            req.headers.origin
+        );
+        const originUrl =
+            req.header('Origin') ||
+            req.get('host') ||
+            req.get('origin') ||
+            req.headers.host ||
+            req.headers.origin;
         for (let i = 0; i < verifiedDomains.length; i++) {
-            if (verifiedDomains[i] === originUrl || verifiedDomains[i].includes(originUrl)) {
+            if (
+                verifiedDomains[i] === originUrl ||
+                verifiedDomains[i].includes(originUrl as string)
+            ) {
                 next();
                 return;
             }
