718e609209d46e5055df995ec3c0e19f153523c7
MomoRazor <maurovic.cachia@gmail.com>
2021-10-31 23:48:36 +0100

    Fixing MiddleWare

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,15 @@ import { SERVICE_PROVIDER_DOMAINS } from './enviornment';
 export const bouncer = (req: Request, res: Response, next: NextFunction) => {
     if (SERVICE_PROVIDER_DOMAINS) {
         const verifiedDomains = SERVICE_PROVIDER_DOMAINS.split(',');
-        if (verifiedDomains.includes(req.headers.referer || '')) {
-            next();
-        } else {
-            console.error('Locked to specific Service Provider', req.headers.referer);
-            res.status(401).end('Locked to specific Service Provider');
+        const originUrl = req.header('Origin') || req.get('host') || req.hostname;
+        for (let i = 0; i < verifiedDomains.length; i++) {
+            if (verifiedDomains[i] === originUrl || verifiedDomains[i].includes(originUrl)) {
+                next();
+                return;
+            }
         }
+        console.error('Locked to specific Service Provider', originUrl);
+        res.status(401).end('Locked to specific Service Provider');
     } else {
         next();
     }
