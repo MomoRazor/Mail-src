1323c37c097d65e59bd8cb9b2916e887ca4fe421
MomoRazor <maurovic.cachia@gmail.com>
2021-10-31 23:38:38 +0100

    Fixing referer

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,10 @@ import { SERVICE_PROVIDER_DOMAINS } from './enviornment';
 export const bouncer = (req: Request, res: Response, next: NextFunction) => {
     if (SERVICE_PROVIDER_DOMAINS) {
         const verifiedDomains = SERVICE_PROVIDER_DOMAINS.split(',');
-        if (verifiedDomains.includes(req.header('Origin') || '')) {
+        if (verifiedDomains.includes(req.headers.referer || '')) {
             next();
         } else {
-            console.error('Locked to specific Service Provider', req.header('Origin'));
+            console.error('Locked to specific Service Provider', req.headers.referer);
             res.status(401).end('Locked to specific Service Provider');
         }
     } else {
