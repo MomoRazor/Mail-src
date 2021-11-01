af5b851442ccd70bdb9c64a67ba75b1693aee913
MomoRazor <maurovic.cachia@gmail.com>
2021-11-01 01:15:22 +0100

    Fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { SERVICE_PROVIDER_DOMAINS } from './enviornment';
+import { API_KEY } from './enviornment';
 
 export const bouncer = (req: Request, res: Response, next: NextFunction) => {
-    if (SERVICE_PROVIDER_DOMAINS) {
-        const verifiedDomains = SERVICE_PROVIDER_DOMAINS.split(',');
-        console.log(
-            req.header('Origin'),
-            req.get('host'),
-            req.get('origin'),
-            req.headers.host,
-            req.headers.origin
-        );
-        const originUrl =
-            req.header('Origin') ||
-            req.get('host') ||
-            req.get('origin') ||
-            req.headers.host ||
-            req.headers.origin;
-        for (let i = 0; i < verifiedDomains.length; i++) {
-            if (
-                verifiedDomains[i] === originUrl ||
-                verifiedDomains[i].includes(originUrl as string)
-            ) {
-                next();
-                return;
-            }
+    if (API_KEY) {
+        if (API_KEY === req.headers.authorization) {
+            next();
+        } else {
+            console.error('Missing Token');
+            res.status(401).end('Missing Token');
         }
-        console.error('Locked to specific Service Provider', originUrl);
-        res.status(401).end('Locked to specific Service Provider');
     } else {
         next();
     }
