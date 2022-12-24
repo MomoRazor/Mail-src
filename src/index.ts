971e7394b31dd7dc1698795a11a779eb102832ff
Maurovic <maurovic.cachia@gmail.com>
2022-12-24 15:07:18 +0000

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,6 @@ import mongoose from 'mongoose';
 import { MAILJET_KEY, MAILJET_SECRET, MONGO_URL, PORT, USES } from './env';
 
 const main = async () => {
-    console.log(MONGO_URL);
-
     // Init database
     const databaseConnection = mongoose.createConnection(MONGO_URL);
 
