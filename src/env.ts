efa42f7d206e0f16d8f109a336823b74025dccc1
Maurovic <maurovic.cachia@gmail.com>
2022-12-24 17:23:02 +0100

    Better Errors

--- src/env.ts
+++ src/env.ts
@@ -6,7 +6,7 @@ if (!process.env.MONGO_URL) throw new Error(`Missing environment variable MONGO_
 export const MONGO_URL = process.env.MONGO_URL;
 
 if (!process.env.RBAC_SECRET && !process.env.API_KEY)
-    throw new Error(`Missing both RBAC_SECRET and APIK_KEY environment variables!`);
+    throw new Error(`Missing both RBAC_SECRET and API_KEY environment variables!`);
 
 export const RBAC_SECRET = process.env.RBAC_SECRET;
 export const API_KEY = process.env.API_KEY;
